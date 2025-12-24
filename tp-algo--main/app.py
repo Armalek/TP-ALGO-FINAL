a76fb5afaa863bc09c9f8789e555155ac6e47706
Armalek <arabmalek635@gmail.com>
2025-12-24 22:42:23 +0100

    Add requirements.txt for Render deployment

--- tp-algo--main/app.py
+++ tp-algo--main/app.py
@@ -1,6 +1,6 @@
 
 #       APP TP1 + TP2 + TP3
-
+import os 
 from flask import Flask, render_template, request, jsonify
 
 import networkx as nx
@@ -396,5 +396,7 @@ def tp4_rlf_draw():
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+   port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
